c77fbc4144d1d9d3afa80df4c4445bdeed807bea
liuyc1515 <liuyicheng1515@sjtu.edu.cn>
2023-09-22 16:51:12 +0000

    implement interface Task in class BaseTask

--- server/src/main/java/org/elasticsearch/tasks/Task.java
+++ server/src/main/java/org/elasticsearch/tasks/Task.java
@@ -65,52 +65,6 @@ public interface Task {
         X_ELASTIC_PRODUCT_ORIGIN_HTTP_HEADER
     );
 
-    private final long id;
-
-    private final String type;
-
-    private final String action;
-
-    private final String description;
-
-    private final TaskId parentTask;
-
-    private final Map<String, String> headers;
-
-    /**
-     * The task's start time as a wall clock time since epoch ({@link System#currentTimeMillis()} style).
-     */
-    private final long startTime;
-
-    /**
-     * The task's start time as a relative time ({@link System#nanoTime()} style).
-     */
-    private final long startTimeNanos;
-
-    public Task(long id, String type, String action, String description, TaskId parentTask, Map<String, String> headers) {
-        this(id, type, action, description, parentTask, System.currentTimeMillis(), System.nanoTime(), headers);
-    }
-
-    public Task(
-        long id,
-        String type,
-        String action,
-        String description,
-        TaskId parentTask,
-        long startTime,
-        long startTimeNanos,
-        Map<String, String> headers
-    ) {
-        this.id = id;
-        this.type = type;
-        this.action = action;
-        this.description = description;
-        this.parentTask = parentTask;
-        this.startTime = startTime;
-        this.startTimeNanos = startTimeNanos;
-        this.headers = headers;
-    }
-
     /**
      * Build a version of the task status you can throw over the wire and back
      * to the user.
@@ -121,83 +75,43 @@ public interface Task {
      *            should the information include detailed, potentially slow to
      *            generate data?
      */
-    public final TaskInfo taskInfo(String localNodeId, boolean detailed) {
-        String description = null;
-        Task.Status status = null;
-        if (detailed) {
-            description = getDescription();
-            status = getStatus();
-        }
-        return taskInfo(localNodeId, description, status);
-    }
+    public TaskInfo taskInfo(String localNodeId, boolean detailed);
 
-    /**
-     * Build a proper {@link TaskInfo} for this task.
-     */
-    protected final TaskInfo taskInfo(String localNodeId, String description, Status status) {
-        return new TaskInfo(
-            new TaskId(localNodeId, getId()),
-            getType(),
-            getAction(),
-            description,
-            status,
-            startTime,
-            System.nanoTime() - startTimeNanos,
-            this instanceof CancellableTask,
-            this instanceof CancellableTask && ((CancellableTask) this).isCancelled(),
-            parentTask,
-            headers
-        );
-    }
 
     /**
      * Returns task id
      */
-    public long getId() {
-        return id;
-    }
+    public long getId();
 
     /**
      * Returns task channel type (netty, transport, direct)
      */
-    public String getType() {
-        return type;
-    }
+    public String getType();
 
     /**
      * Returns task action
      */
-    public String getAction() {
-        return action;
-    }
+    public String getAction();
 
     /**
      * Generates task description
      */
-    public String getDescription() {
-        return description;
-    }
+    public String getDescription();
 
     /**
      * Returns the task's start time as a wall clock time since epoch ({@link System#currentTimeMillis()} style).
      */
-    public long getStartTime() {
-        return startTime;
-    }
+    public long getStartTime();
 
     /**
      * Returns the task's start time in nanoseconds ({@link System#nanoTime()} style).
      */
-    public long getStartTimeNanos() {
-        return startTimeNanos;
-    }
+    public long getStartTimeNanos();
 
     /**
      * Returns id of the parent task or NO_PARENT_ID if the task doesn't have any parent tasks
      */
-    public TaskId getParentTaskId() {
-        return parentTask;
-    }
+    public TaskId getParentTaskId();
 
     /**
      * Build a status for this task or null if this task doesn't have status.
@@ -205,28 +119,10 @@ public interface Task {
      * this can never perform IO it might be a costly operation, requiring
      * collating lists of results, etc. So only use it if you need the value.
      */
-    public Status getStatus() {
-        return null;
-    }
+    public Status getStatus();
 
     @Override
-    public String toString() {
-        return "Task{id="
-            + id
-            + ", type='"
-            + type
-            + "', action='"
-            + action
-            + "', description='"
-            + description
-            + "', parentTask="
-            + parentTask
-            + ", startTime="
-            + startTime
-            + ", startTimeNanos="
-            + startTimeNanos
-            + '}';
-    }
+    public String toString();
 
     /**
      * Report of the internal status of a task. These can vary wildly from task
@@ -245,23 +141,11 @@ public interface Task {
     /**
      * Returns stored task header associated with the task
      */
-    public String getHeader(String header) {
-        return headers.get(header);
-    }
+    public String getHeader(String header);
 
-    public Map<String, String> headers() {
-        return headers;
-    }
+    public Map<String, String> headers();
 
-    public TaskResult result(DiscoveryNode node, Exception error) throws IOException {
-        return new TaskResult(taskInfo(node.getId(), true), error);
-    }
+    public TaskResult result(DiscoveryNode node, Exception error) throws IOException;
 
-    public TaskResult result(DiscoveryNode node, ActionResponse response) throws IOException {
-        if (response instanceof ToXContent) {
-            return new TaskResult(taskInfo(node.getId(), true), (ToXContent) response);
-        } else {
-            throw new IllegalStateException("response has to implement ToXContent to be able to store the results");
-        }
-    }
+    public TaskResult result(DiscoveryNode node, ActionResponse response) throws IOException;
 }
