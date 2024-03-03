9a903d0cc6fd4a2249c21d52d85478502c8ab827
liuyc <liuyicheng1515@sjtu.edu.cn>
2024-03-03 20:17:05 +0800

    update build_and_run: set elasticsearch uid in docker compose in elasticsearch_exp, compose files in cases to be updated

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,11 +1,9 @@
-root_dir=/home/eason/elasticsearch_proj
-user_name=eason
+root_dir=/home/eason/AutoCancelProject/
 
-DEFAULT_POLICY=multi_objective_policy PREDICT_PROGRESS=false docker compose -f $root_dir/autocancel_exp/elasticsearch_exp/cluster/single_node.yml down && \
-sudo chown -R $user_name:$user_name $root_dir/elasticsearch/build/distribution/local/elasticsearch-8.9.0-SNAPSHOT && \
+USER_ID=$(id -u) GROUP_ID=$(id -g) AUTOCANCEL_HOME=$root_dir DEFAULT_POLICY=multi_objective_policy PREDICT_PROGRESS=false docker compose -f $root_dir/autocancel_exp/elasticsearch_exp/cluster/single_node.yml down && \
+sudo chown -R $(id -u):$(id -g) $root_dir/elasticsearch/build/distribution/local/elasticsearch-8.9.0-SNAPSHOT && \
 curr_wd=$(pwd) && \
 cd $root_dir/elasticsearch && \
 ./gradlew localDistro && \
 cd $curr_wd && \
-sudo chown -R 1000:1000 $root_dir/elasticsearch/build/distribution/local/elasticsearch-8.9.0-SNAPSHOT && \
-DEFAULT_POLICY=multi_objective_policy PREDICT_PROGRESS=false docker compose -f $root_dir/autocancel_exp/elasticsearch_exp/cluster/single_node.yml up
+USER_ID=$(id -u) GROUP_ID=$(id -g) AUTOCANCEL_HOME=$root_dir DEFAULT_POLICY=multi_objective_policy PREDICT_PROGRESS=false docker compose -f $root_dir/autocancel_exp/elasticsearch_exp/cluster/single_node.yml up
